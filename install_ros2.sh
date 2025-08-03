0aaaf187f3cc5202e7976bd3ecd7b4cecface3d8
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2025-08-03 10:15:00 +0100

    Update install_ros2.sh

--- install_ros2.sh
+++ install_ros2.sh
@@ -11,9 +11,11 @@ export ROS_APT_SOURCE_VERSION=$(curl -s https://api.github.com/repos/ros-infrast
 curl -L -o /tmp/ros2-apt-source.deb "https://github.com/ros-infrastructure/ros-apt-source/releases/download/${ROS_APT_SOURCE_VERSION}/ros2-apt-source_${ROS_APT_SOURCE_VERSION}.$(. /etc/os-release && echo $VERSION_CODENAME)_all.deb" # If using Ubuntu derivates use $UBUNTU_CODENAME
 sudo dpkg -i /tmp/ros2-apt-source.deb
 
-sudo apt update && sudo apt upgrade -y
-
+sudo apt update
 sudo apt install -y ros-jazzy-desktop ros-dev-tools
+
+# Remove APT files
+rm -rf /var/cache/apt/archives /var/lib/apt/lists/*
 
 echo "# Source ROS2 Jazzy, as instructed in https://ros2-tutorial.readthedocs.io" >> ~/.bashrc
 echo "source /opt/ros/jazzy/setup.bash" >> ~/.bashrc
